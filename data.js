9fea6386494ce66bdec033fe9010bf3b5ff03483
3DesignRootsVR <3designrootscreativecloud@gmail.com>
2025-11-06 17:49:53 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bed-space",
+      "name": "BED SPACE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5245464649356943,
+          "pitch": 0.014753083005439649,
+          "rotation": 4.71238898038469,
+          "target": "1-dressing-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dressing-area",
+      "name": "DRESSING AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "MR PATIL BED ROOM B",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
